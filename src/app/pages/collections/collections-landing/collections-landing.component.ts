c142f986a6b291acb50045a8ad35507852b09c27
Sherwin S. Abion <Sherwin.Abion@CONVERGEICT.COM>
2025-03-19 14:21:47 +0800

    collections view template

--- src/app/pages/collections/collections-landing/collections-landing.component.ts
+++ src/app/pages/collections/collections-landing/collections-landing.component.ts
@@ -14,43 +14,43 @@ export class CollectionsLandingComponent {
   collectionItems: IImageList[] = [
     {
       image: '../../../../../../assets/images/collections/collection1.jpg',
-      title: 'Nature',
-      url: '/test',
+      title: 'Natures Venture',
+      url: 'view/natures-venture',
     },
     {
       image: '../../../../../../assets/images/collections/collection2.jpg',
-      title: 'Nature',
-      url: '/test',
+      title: 'Sweet and Spice',
+      url: 'view/sweet-and-spice',
     },
     {
       image: '../../../../../../assets/images/collections/collection3.jpg',
-      title: 'Nature',
-      url: '/test',
+      title: 'Fresh and Drip',
+      url: 'view/fresh-and-drip',
     },
     {
       image: '../../../../../../assets/images/collections/collection4.jpg',
-      title: 'Nature',
-      url: '/test',
+      title: 'Blush Collections',
+      url: 'view/blush-collections',
     },
     {
       image: '../../../../../../assets/images/collections/collection5.jpg',
       title: 'Nature',
-      url: '/test',
+      url: 'view',
     },
     {
       image: '../../../../../../assets/images/collections/collection6.jpg',
       title: 'Nature',
-      url: '/test',
+      url: 'view',
     },
     {
       image: '../../../../../../assets/images/collections/collection7.jpg',
       title: 'Nature',
-      url: '/test',
+      url: 'view',
     },
     {
-      image: '../../../../../../assets/images/collections/collection8.jpg',
+      image: '../../../../../../assets/images/collections/collection1.jpg',
       title: 'Nature',
-      url: '/test',
+      url: 'view',
     },
   ];
 }
--- src/app/shared/components/images/images-category/images-category.component.ts
+++ src/app/shared/components/images/images-category/images-category.component.ts
@@ -1,10 +1,12 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
 import { IImageList } from '../../../interfaces/carousel.inferface';
 
 @Component({
   selector: 'app-images-category',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './images-category.component.html',
   styleUrl: './images-category.component.scss',
 })
